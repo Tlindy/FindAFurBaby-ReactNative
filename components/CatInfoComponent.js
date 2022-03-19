b895508d7f44fdb1ca331debc80108331a491058
Teresa L <tml2009jghs@yahoo.com>
2022-03-18 21:28:14 -0400

    Adopt Info Contact Modal

--- components/CatInfoComponent.js
+++ components/CatInfoComponent.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card, Image } from "react-native-elements";
+import {
+    Text,
+    View,
+    ScrollView,
+    Button,
+    Modal,
+    StyleSheet
+} from "react-native";
+import { Card, Input, CheckBox } from "react-native-elements";
 import { CATS } from "../shared/catsAdopt";
 
-export function RenderCat({ cat }) {
+export function RenderCat(props) {
+    const { cat } = props;
+
     if (cat) {
         return (
             <Card
@@ -14,6 +23,13 @@ export function RenderCat({ cat }) {
                     Name: {cat.name}
                 </Text>
                 <Text style={{ margin: 10, fontSize: 15 }}>{cat.info}</Text>
+                <View>
+                    <Button
+                        title="Adopt"
+                        color="lightpink"
+                        onPress={() => props.onShowModal()}
+                    />
+                </View>
             </Card>
         );
     }
@@ -25,7 +41,26 @@ class CatInfo extends Component {
         super(props);
         this.state = {
             cats: CATS,
+            showModal: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            agree: false
         };
+    }
+
+    toggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    resetForm() {
+        this.setState({
+            showModal: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            agree: false
+        });
     }
 
     static navigationOptions = {
@@ -35,8 +70,92 @@ class CatInfo extends Component {
     render() {
         const catId = this.props.navigation.getParam("catId");
         const cat = this.state.cats.filter((cat) => cat.id === catId)[0];
-        return <RenderCat cat={cat} />;
+        return (
+            <ScrollView>
+                <RenderCat 
+                    cat={cat}
+                    onShowModal={() => this.toggleModal()}
+                />
+                <Modal
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}
+                >
+                    <View style={styles.modal}>
+                        <Text style={styles.modalText}>
+                            Please fill out the form below to receive an adoption form and more information about adopting from Find A Fur Baby
+                        </Text>
+                        <Input
+                            placeholder="First Name"
+                            onChangeText={text => 
+                                this.setState({ firstName: text })
+                            }
+                            value={this.state.firstName}
+                        />
+                        <Input
+                            placeholder="Last Name"
+                            onChangeText={text => 
+                                this.setState({ lastName: text })
+                            }
+                            value={this.state.lastName}
+                        />
+                        <Input
+                            placeholder="Email"
+                            onChangeText={text => 
+                                this.setState({ email: text })
+                            }
+                            value={this.state.email}
+                        />
+                        <CheckBox
+                            title="I agree to receive email from Find A Fur Baby"
+                            center
+                            checked={this.state.agree}
+                            onPress={() => this.setState({agree: !this.state.agree})}
+                            containerStyle={styles.formCheckbox}
+                        />
+                        <View style={styles.modalButtons}>
+                            <Button
+                                title="Submit"
+                                color="lightskyblue"
+                                onPress={() => this.resetForm()}
+                            />
+                            <Button
+                                title="Cancel"
+                                color="grey"
+                                onPress={() => {
+                                    this.toggleModal();
+                                    this.resetForm();
+                                }}
+                            />
+                        </View>
+                    </View>
+                </Modal>
+            </ScrollView>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    modal: {
+        justifyContent: "center",
+        margin: 20
+    },
+    modalText : {
+        fontSize: 24,
+        paddingBottom: 40,
+        color: "lightpink",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    formCheckbox: {
+        margin: 10,
+        backgroundColor: null
+    },
+    modalButtons: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        paddingTop: 10
+    }
+})
 
 export default CatInfo;
--- components/DogInfoComponent.js
+++ components/DogInfoComponent.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card, Image } from "react-native-elements";
+import {
+    Text,
+    View,
+    ScrollView,
+    Button,
+    Modal,
+    StyleSheet
+} from "react-native";
+import { Card, Input, CheckBox } from "react-native-elements";
 import { DOGS } from "../shared/dogsAdopt";
 
-export function RenderDog({ dog }) {
+export function RenderDog(props) {
+    const { dog } = props;
+
     if (dog) {
         return (
             <Card
@@ -16,7 +25,16 @@ export function RenderDog({ dog }) {
                 <Text style={{ marginLeft: 10, fontSize: 20 }}>
                     Breed: {dog.breed}
                 </Text>
-                <Text style={{ margin: 10, fontSize: 15 }}>{dog.info}</Text>
+                <Text style={{ margin: 10, fontSize: 15 }}>
+                    {dog.info}
+                </Text>
+                <View>
+                    <Button
+                        title="Adopt"
+                        color="lightpink"
+                        onPress={() => props.onShowModal()}
+                    />
+                </View>
             </Card>
         );
     }
@@ -28,7 +46,26 @@ class DogInfo extends Component {
         super(props);
         this.state = {
             dogs: DOGS,
+            showModal: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            agree: false
         };
+    }
+
+    toggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    resetForm() {
+        this.setState({
+            showModal: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            agree: false
+        });
     }
 
     static navigationOptions = {
@@ -38,8 +75,92 @@ class DogInfo extends Component {
     render() {
         const dogId = this.props.navigation.getParam("dogId");
         const dog = this.state.dogs.filter((dog) => dog.id === dogId)[0];
-        return <RenderDog dog={dog} />;
+        return (
+            <ScrollView>
+                <RenderDog 
+                    dog={dog}
+                    onShowModal={() => this.toggleModal()}
+                />
+                <Modal
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}
+                >
+                    <View style={styles.modal}>
+                        <Text style={styles.modalText}>
+                            Please fill out the form below to receive an adoption form and more information about adopting from Find A Fur Baby
+                        </Text>
+                        <Input
+                            placeholder="First Name"
+                            onChangeText={text => 
+                                this.setState({ firstName: text })
+                            }
+                            value={this.state.firstName}
+                        />
+                        <Input
+                            placeholder="Last Name"
+                            onChangeText={text => 
+                                this.setState({ lastName: text })
+                            }
+                            value={this.state.lastName}
+                        />
+                        <Input
+                            placeholder="Email"
+                            onChangeText={text => 
+                                this.setState({ email: text })
+                            }
+                            value={this.state.email}
+                        />
+                        <CheckBox
+                        title="I agree to receive email from Find A Fur Baby"
+                        center
+                        checked={this.state.agree}
+                        onPress={() => this.setState({agree: !this.state.agree})}
+                        containerStyle={styles.formCheckbox}
+                        />
+                        <View style={styles.modalButtons}>
+                            <Button
+                                title="Submit"
+                                color="lightskyblue"
+                                onPress={() => this.resetForm()}
+                            />
+                            <Button
+                                title="Cancel"
+                                color="grey"
+                                onPress={() => {
+                                    this.toggleModal();
+                                    this.resetForm();
+                                }}
+                            />
+                        </View>
+                    </View>
+                </Modal>
+            </ScrollView>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+        modal: {
+            justifyContent: "center",
+            margin: 20
+        },
+        modalText : {
+            fontSize: 24,
+            paddingBottom: 40,
+            color: "lightpink",
+            fontWeight: "bold",
+            textAlign: "center"
+        },
+        formCheckbox: {
+            margin: 10,
+            backgroundColor: null
+        },
+        modalButtons: {
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            paddingTop: 10
+        }
+})
 
 export default DogInfo;
